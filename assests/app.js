eaab79daded075fe16d15724f53ab154895ae909
Tari Namikpoh <tarinamikpoh@gmail.com>
2023-11-26 09:00:42 +0100

    finished the check box

--- assests/app.js
+++ assests/app.js
@@ -4,24 +4,41 @@ function Remove() {
 }
 function Focus(e) {
     var arrow = document.querySelector('.arrow')
+
     e.target.style.borderRaduis = '6px'
     arrow.classList.toggle('active')
+
 
 }
 
 var counter = document.querySelector('.counter')
 var checkBox = document.querySelectorAll('.check-box')
+var checkBoxIcon = document.querySelectorAll('.checkbox')
+var checked = document.querySelectorAll('.checked')
 var number = 0;
 counter.textContent = number
 var hasBeenClicked = false;
 checkBox.forEach( checkBoxes => {
     checkBoxes.addEventListener("click", (event)=>{
         var bar = document.querySelector('.progress-bar')
+        var rollingAnimation = document.querySelectorAll('.rolling-animation')
         counter.textContent = number
         bar.style.setProperty('--width', '25px');
         if (!hasBeenClicked) {
             if (number < 5) {
                 number = number + 1
+                checkBoxIcon.forEach(checkBoxIcons => {
+                    checkBoxIcons.classList.add('active')
+                });
+                checked.forEach(checkeded => {
+                    checkeded.classList.add('active')
+                });
+                rollingAnimation.forEach(rollingAnimations => {
+                    rollingAnimations.classList.toggle('active')
+                });
+               
+               
+                
             }
             counter.textContent = number
             // hasBeenClicked = true;
@@ -29,8 +46,15 @@ checkBox.forEach( checkBoxes => {
             if (number < 5) {
                 number = number - 1
             }
+            checkBoxIcon.forEach(checkBoxIcons => {
+                checkBoxIcons.classList.remove('active')
+            });
+            checked.forEach(checkeded => {
+                checkeded.classList.remove('active')
+            });
             counter.textContent = number
             bar.style.setProperty('--width', '0px');
+            
             // hasBeenClicked = true;
         }
         
